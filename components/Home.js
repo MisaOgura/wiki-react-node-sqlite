81df5994b5f3ae6c0b2b42dd4f5ba9d40d48b10a
MisaOgura <misa.ogura01@gmail.com>
2017-11-01 18:50:32 +0000

    Introduce bootstrap

--- components/Home.js
+++ components/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return <div className='wikis-index'>
     <h1>Wiki Index</h1>
-    <Link to='/wikis/new' className='create-wiki' >Create a new wiki</Link>
+    <Link to='/wikis/new' className='btn btn-default btn-lg create-wiki' >Create a new wiki</Link>
   </div>
 }
 

96cab45d379fe8e91140cf2a3cfd22ccf69d5ded
MisaOgura <misa.ogura01@gmail.com>
2017-11-03 12:21:09 +0000

    Separate routes into App component

---
+++ components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+import Home from './Home'
+import NewWiki from './NewWiki'
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path='/wikis/new' component={NewWiki}/>
+      <Route exact path='/' component={Home}/>
+    </Switch>
+  )
+}
+
+export default App
--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-import Home from './components/Home'
-import NewWiki from './components/NewWiki'
+import App from './components/App'
 
 render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path='/wikis/new' component={NewWiki} />
-      <Route exact path='/' component={Home} />
-    </Switch>
-  </BrowserRouter>,
+  <Router><App /></Router>,
   document.getElementById('app')
 )

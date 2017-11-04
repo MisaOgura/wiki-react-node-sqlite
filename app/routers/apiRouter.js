4065f2d60eccc8f18d165ca4a29d0e325780e687
MisaOgura <misa.ogura01@gmail.com>
2017-11-04 18:00:00 +0000

    Create an endpoint to serve list of entries

--- app/routers/apiRouter.js
+++ app/routers/apiRouter.js
@@ -4,10 +4,9 @@ import path from 'path'
 import Sql from 'better-sqlite3'
 
 const apiRouter = Router()
+const env = process.env.NODE_ENV
 
 apiRouter.post('/wikis', (req, res) => {
-  const env = process.env.NODE_ENV
-
   const title = req.body.title
   const content = req.body.content
   const now = moment().format()
@@ -19,6 +18,17 @@ apiRouter.post('/wikis', (req, res) => {
 
   const latestEntry = db.prepare('SELECT * FROM wiki ORDER BY date_created DESC LIMIT 1').get()
   res.status(200).send(latestEntry)
+
+  db.close()
+})
+
+apiRouter.get('/wikis', (req, res) => {
+  const db = new Sql(path.join(__dirname, `../../db/${env}.db`))
+  const query = 'SELECT * FROM wiki ORDER BY datetime(date_created) DESC'
+  const entries = db.prepare(query).all()
+
+  res.status(200).send(entries)
+
   db.close()
 })
 

ea254e39cb3182b2489279e4565805eeebe32c2c
MisaOgura <misa.ogura01@gmail.com>
2017-11-05 21:42:01 +0000

    Create a db dir if it doesn't exit

--- app/services/databaseMiddleware.js
+++ app/services/databaseMiddleware.js
@@ -1,9 +1,16 @@
+import fs from 'fs'
 import path from 'path'
 import Sql from 'better-sqlite3'
 
+const dbDirPath = path.join(__dirname, '../db')
+
+if (!fs.existsSync(dbDirPath)){
+  fs.mkdirSync(dbDirPath)
+}
+
 const databaseMiddleware = (req, res, next) => {
   const env = process.env.NODE_ENV
-  const db = new Sql(path.join(__dirname, `../db/${env}.db`))
+  const db = new Sql(path.join(dbDirPath, `${env}.db`))
 
   db.prepare('CREATE TABLE  IF NOT EXISTS wiki (' +
     'id INTEGER PRIMARY KEY AUTOINCREMENT, ' +

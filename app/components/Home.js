144e03ab35e426c9f27c1acbea69abbc14488cf2
MisaOgura <misa.ogura01@gmail.com>
2017-11-05 09:22:06 +0000

    Render a list of entries on the home page

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { ListGroup, ListGroupItem } from 'react-bootstrap'
 
 class Home extends Component {
   constructor (props) {
@@ -7,10 +7,28 @@ class Home extends Component {
     this.state = {entries: this.props.preloadedData || []}
   }
 
+  renderWikiListItem (entry) {
+    return <ListGroupItem
+      className='wiki-list-item'
+      key={entry.id}
+      href={`/wikis/${entry.id}`}
+      onClick={() => {}}
+      header={entry.title}>
+      {`Created: ${formatDate(entry.date_created)}`}
+    </ListGroupItem>
+
+    function formatDate (date) {
+      return new Date(date).toLocaleString()
+    }
+  }
+
   render () {
-    return <div className='wikis-index'>
+    return <div className='home'>
       <h1>Wiki Index</h1>
-      <Link to='/wikis/new' className='btn btn-default btn-lg home' >Create a new wiki</Link>
+      {/* <Link to='/wikis/new' className='btn btn-default btn-lg link-create' >Create a new wiki</Link> */}
+      <ListGroup>
+        {this.state.entries.map(this.renderWikiListItem)}
+      </ListGroup>
     </div>
   }
 }
--- app/components/Home.test.js
+++ app/components/Home.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-import { Link } from 'react-router-dom'
+import { mount } from 'enzyme'
 
 import Home from './Home'
 
@@ -8,15 +7,37 @@ describe('Home', () => {
   let home
 
   beforeEach(() => {
-    home = shallow(<Home />)
+    home = mount(<Home />)
   })
 
-  it('displays a div with a className wikis-index', () => {
-    expect(home.hasClass('wikis-index')).toEqual(true)
-  })
+  it('displays a list of existing wikis with the correct props', () => {
+    const entryOne = {
+      id: 1,
+      title: 'Planet Ant',
+      content: 'They are powerful',
+      date_created: '2017-11-03T22:13:15+00:00',
+      date_updated: '2017-11-03T22:08:06+00:00'
+    }
+    const entryTwo = {
+      id: 2,
+      title: 'Africa',
+      content: 'Migration of wildebeest',
+      date_created: '2017-11-02T22:13:15+00:00',
+      date_updated: '2017-11-04T22:08:06+00:00'
+    }
+    const entries = [entryOne, entryTwo]
 
-  it('displays a link to /wikis/new', () => {
-    const linkToNewWiki = home.find(Link).find('.home')
-    expect(linkToNewWiki.prop('to')).toEqual('/wikis/new')
+    home.setState({entries})
+
+    home.find('.wiki-list-item').hostNodes().forEach((listItem, i) => {
+      const entry = entries[i]
+      const dateCreated = new Date(entry.date_created).toLocaleString()
+
+      expect(listItem.find(`a[href="/wikis/${entry.id}"]`)).toHaveLength(1)
+      expect(listItem.find('.list-group-item-heading').text()).toEqual(`${entry.title}`)
+      expect(listItem.find('.list-group-item-text').text()).toEqual(`Created: ${dateCreated}`)
+    })
+
+    expect(home.find('.wiki-list-item').hostNodes()).toHaveLength(entries.length)
   })
 })
